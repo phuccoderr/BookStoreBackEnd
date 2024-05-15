5f44a9bab7fa3c0774b36b58f2047514b620a959
phuccoderr <phuctapcode@gmail.com>
2024-05-16 03:28:55 +0700

    reload sln

--- ProjectBook/obj/Debug/net8.0/ProjectBook.AssemblyInfo.cs
+++ ProjectBook/obj/Debug/net8.0/ProjectBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33650a864a1314d18123e0566518e033ea6de830")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1283589d9fd45a7465e2784d7b0ca7a6d7c20c94")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
